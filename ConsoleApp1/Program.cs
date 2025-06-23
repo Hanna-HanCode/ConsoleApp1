81ff13b695efbfd7c7ccb89249f188f83664f534
Hanna-SS <aninha.souza123@gmail.com>
2025-06-23 19:53:22 -0300

    Nova pratica, cadastro de animais, pratica de herança e override

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Estruturas_de_repeticao.Executar();
+            Cadastro_Animais.Executar();
         }
     }
 }
---
+++ ConsoleApp1/Testes/Cadastro_Animais.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Testes
+{
+
+    //Classe abstrata
+    abstract class Animal
+    {
+        public string Nome;
+        public int Idade;
+
+        public Animal(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+        public abstract string EmitirSom();
+
+        public virtual string ExibirInfo()
+        {
+            return $"Nome: {Nome}, Idade: {Idade}";
+
+        }
+    }
+        class Cachorro : Animal {
+            public Cachorro(string nome, int idade) : base(nome, idade) { }
+
+            public override string EmitirSom()
+            {
+                return "Au au!";
+            }
+            public override string ExibirInfo()
+            {
+                return base.ExibirInfo() + "\nEsse animal é um cachorro.";
+            }
+        }
+        class Gato : Animal
+        {
+            public Gato(string nome, int idade) : base(nome, idade) { }
+
+            public override string EmitirSom()
+            {
+                return "Miau!";
+            }
+            public override string ExibirInfo()
+            {
+                return base.ExibirInfo() + "\nEsse animal é um gato.";
+            }
+        }
+    internal class Cadastro_Animais
+    {
+        public static void Executar()
+        {
+            Cachorro cachorro = new Cachorro("Rex", 2);
+            Gato gato = new Gato("Clébinho", 5);
+
+            Console.WriteLine(cachorro.ExibirInfo());
+            Console.WriteLine(cachorro.EmitirSom());
+            Console.WriteLine();
+            Console.WriteLine(gato.ExibirInfo());
+            Console.WriteLine(gato.EmitirSom());
+        }
+    }
+}
